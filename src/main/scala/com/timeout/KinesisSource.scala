db4f76c894c7acf5adf5cdc83f0775d5b9f2334c
tomverran <tom@tomverran.co.uk>
2017-02-21 16:46:12 +0000

    Remove unimplemented classes

--- src/main/scala/com/timeout/KinesisSource.scala
+++
@@ -1,9 +0,0 @@
-package com.timeout
-
-import akka.NotUsed
-import akka.stream.scaladsl.Source
-import com.amazonaws.services.kinesis.model.Record
-
-object KinesisSource {
-  def apply(): Source[Record, NotUsed] = ???
-}
